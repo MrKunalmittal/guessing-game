dd713d2f50ece7aa1fee375efe8e394918441242
MrKunalmittal <kunalmittal19498@gmail.com>
2022-11-04 10:36:42 -0400

    Guessing Game

---
+++ assests/script/index.js
@@ -0,0 +1,75 @@
+'use strict';
+/*
+    java script
+
+    Guessing game
+
+*/
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function getElement(selector, parent = document) {
+    return parent.getElementById(selector);
+
+}
+
+let msg1 = select('.message1');
+let msg2 = select('.message2');
+let msg3 = select('.message3');
+let userGuess = select('.guess').value;
+const btn = select('.my_btn');
+const restart= select('.restart');
+
+
+let answer = Math.floor(Math.random() * 100) + 1;
+let noOfGuess = 8;
+let guessedNums = [];
+
+
+onEvent('click', btn, function () {
+
+    let userGuess = select('.guess').value;
+    
+    if (userGuess < 1 || userGuess > 100) {
+        alert('Please Enter number between 1 to 100');
+    }
+    else {
+        guessedNums.push(userGuess);
+        noOfGuess -= 1;
+
+        if (userGuess > answer && noOfGuess >= 0) {
+            msg1.innerText = "Your guess is too low.";
+            msg2.innerText = "No. of guesses left : " + noOfGuess;
+            msg3.innerText = "Guessed numbers are: " + guessedNums;
+        } else if (userGuess < answer && noOfGuess >= 0) {
+            msg1.innerText = "Your guess is too high.";
+            msg2.innerText = "No. of guesses left: " + noOfGuess;
+            msg3.innerText = "Guessed numbers are: " + guessedNums;
+        } else if (userGuess == answer && noOfGuess >= 0) {
+            msg1.innerText = "You Won ";
+            msg2.innerText = "The number was: " + answer;
+            msg3.innerText = "You Guessed numbers in  " + noOfGuess + "guesses";
+            userGuess.value = ' ';
+            
+            
+
+        } else if ( noOfGuess = 0 && userGuess == answer){
+            alert('Play Again !');
+        }
+    }
+    
+});
+
+onEvent('click', restart , function(){
+    if(noOfGuess == 0 || userGuess == answer){
+        //msg1.innerText = "Play Again ";
+        // msg2.innerText = "The number was: " + answer;
+        // msg3.innerText = "You Guessed numbers in  " + noOfGuess + "guesses";
+        userGuess.value = ' ';
+    }
+});
+
